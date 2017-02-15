1d5fbe779ea0487f825276aeba021666ed519e3a
TenchuHawke <hawke@unrestrainedgames.com>
2017-02-15 13:11:50 -0800

    Main Game loop functionality installed.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace clickerCoder.Controllers
 {
     public class HomeController : Controller
-    {
+       {
         // GET: /Home/
         [HttpGet]
         [Route("")]
