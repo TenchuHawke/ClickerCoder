18b6e481248caefae08dc51e6268a61affb31a39
Haocheng Yang <haocheng0906@gmail.com>
2017-02-15 13:53:07 -0800

    Haocheng first commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,36 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace clickerCoder.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /Home/
+
         [HttpGet]
         [Route("")]
-        public IActionResult Index()
+        public IActionResult index()
         {
+            int? Number = HttpContext.Session.GetInt32("Number");
+            if(Number == null)
+            {
+                Number = 0;
+            }
+            Number +=1;
+
+            ViewBag.RunNumber = Number;
+            HttpContext.Session.SetInt32("Number", (int)Number);
             return View();
+        }
+
+
+        [HttpGet]
+        [Route("clear")]
+        public IActionResult clear(){
+              HttpContext.Session.Clear();
+              return RedirectToAction ("index");
         }
     }
 }
